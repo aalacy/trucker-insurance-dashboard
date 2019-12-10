0feae879914b86575bca29cf01b19056656dc426
aalacy <aaronlacywhite387@gmail.com>
2019-12-10 06:33:15 -0800

    upload

--- src/api.js
+++ src/api.js
@@ -39,7 +39,7 @@ class APIService {
 
   async request(method, url, data, params, options = {'Content-Type': 'application/json'}) {
     let config = {
-      url: `http://localhost:3000/api/${url}`,
+      url: `https://luckytruck.co/api/${url}`,
       data,
       method,
       params,
